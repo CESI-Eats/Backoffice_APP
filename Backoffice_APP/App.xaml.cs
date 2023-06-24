5fc4e0090d15bdd2dcdd2dff50501bfab2c625af
Helrho <armand-gab@hotmail.fr>
2023-06-24 22:56:09 +0200

    Add project files.

---
+++ Backoffice_APP/App.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Backoffice_APP.ViewModels;
+using StarterKitMvvm;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Backoffice_APP
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly LoginWindow _loginWindow;
+
+        private readonly MainViewModel _mainViewModel;
+        private readonly LoginViewModel _loginViewModel;
+
+        public App()
+        {
+            _loginWindow = new LoginWindow();
+
+            _mainViewModel = new MainViewModel();
+            _loginViewModel = new LoginViewModel();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _loginViewModel.LoginSuccessful += OnLoginSuccessful;
+
+            _loginWindow.DataContext = _loginViewModel;
+            _loginWindow.Show();
+            base.OnStartup(e);
+        }
+
+        private void OnLoginSuccessful()
+        {
+            _loginViewModel.LoginSuccessful -= OnLoginSuccessful;
+            MainWindow = new MainWindow()
+            {
+                DataContext = _mainViewModel
+            };
+
+            _loginWindow.Close();
+            MainWindow.Show();
+        }
+    }
+}
